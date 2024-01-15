b329de43ca2d5395059048cfbc1949d560dbba55
Tyzer33 <78021579+Tyzer33@users.noreply.github.com>
2024-01-16 00:18:08 +0100

    Github Pages

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   base: './',
-  // build: {
-  //   assetsInlineLimit: 0,
-  // },
+  build: {
+    assetsInlineLimit: 0,
+  },
 })
